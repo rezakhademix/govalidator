6ad6fc1f07ff0c7dfe86fd0cdebf8977709da48d
Amir Sadeghi <amirmms2008@gmail.com>
2024-03-04 14:07:04 +0330

    fixed: max defualt messages

--- max.go
+++ max.go
@@ -1,8 +1,22 @@
 package validator
 
+const (
+	// Max represents the rule name which will be used to find the default error message.
+	Max = "max"
+	// MaxMsg is the default error message format for len rule.
+	MaxMsg = "%s should less than %v"
+)
+
 // MaxInt checks i to be less than max value
 func (v *Validator) MaxInt(i, max int, field, msg string) *Validator {
-	v.Check(i <= max, field, msg)
+	v.Check(i <= max, field, v.msg(Max, msg, field, max))
+
+	return v
+}
+
+// MaxFloat64 checks i to be less than max value
+func (v *Validator) MaxFloat64(i, max float64, field, msg string) *Validator {
+	v.Check(i <= max, field, v.msg(Max, msg, field, max))
 
 	return v
 }
--- max_test.go
+++ max_test.go
@@ -27,17 +27,17 @@ func TestValidator_MaxInt(t *testing.T) {
 			field:       "t1",
 			value:       1,
 			max:         0,
-			message:     "t1 must be less than 0",
+			message:     "",
 			isPassed:    false,
-			expectedMsg: "t1 must be less than 0",
+			expectedMsg: "t1 should less than 0",
 		},
 		{
 			field:       "t2",
 			value:       122,
 			max:         20,
-			message:     "t1 must be less than 20",
+			message:     "t2 must be less than 20",
 			isPassed:    false,
-			expectedMsg: "t1 must be less than 20",
+			expectedMsg: "t2 must be less than 20",
 		},
 	}
 
@@ -45,6 +45,54 @@ func TestValidator_MaxInt(t *testing.T) {
 
 	for _, test := range tests {
 		v.MaxInt(test.value, test.max, test.field, test.message)
+
+		assert.Equal(t, test.isPassed, v.IsPassed())
+
+		if !test.isPassed {
+			assert.Equal(t, test.expectedMsg, v.Errors()[test.field])
+		}
+	}
+}
+
+func TestValidator_MaxFloat64(t *testing.T) {
+	tests := []struct {
+		field       string
+		value       float64
+		max         float64
+		message     string
+		isPassed    bool
+		expectedMsg string
+	}{
+		{
+			field:       "t0",
+			value:       10.1,
+			max:         10.8,
+			message:     "",
+			isPassed:    true,
+			expectedMsg: "",
+		},
+		{
+			field:       "t1",
+			value:       0.1,
+			max:         0.01,
+			message:     "",
+			isPassed:    false,
+			expectedMsg: "t1 should less than 0.01",
+		},
+		{
+			field:       "t2",
+			value:       122,
+			max:         20,
+			message:     "t2 must be less than 20",
+			isPassed:    false,
+			expectedMsg: "t2 must be less than 20",
+		},
+	}
+
+	v := New()
+
+	for _, test := range tests {
+		v.MaxFloat64(test.value, test.max, test.field, test.message)
 
 		assert.Equal(t, test.isPassed, v.IsPassed())
 
