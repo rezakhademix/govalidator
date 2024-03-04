8423d701ecabdeaf9b4b6b4f9211d50e58409dd8
Amir Sadeghi <amirmms2008@gmail.com>
2024-03-04 14:38:59 +0330

    added: len slice rule

--- len_test.go
+++ len_test.go
@@ -91,3 +91,50 @@ func TestValidator_LenInt(t *testing.T) {
 		}
 	}
 }
+
+func TestValidator_LenSlice(t *testing.T) {
+	tests := []struct {
+		field       string
+		value       []any
+		len         int
+		message     string
+		isPassed    bool
+		expectedMsg string
+	}{
+		{
+			field:       "t0",
+			value:       []any{1, 2, 3},
+			len:         3,
+			message:     "",
+			isPassed:    true,
+			expectedMsg: "",
+		},
+		{
+			field:       "t1",
+			value:       []any{1.1, 2.2, 7.9},
+			len:         5,
+			message:     "",
+			isPassed:    false,
+			expectedMsg: fmt.Sprintf(LenMsg, "t1", 5),
+		},
+		{
+			field:       "t1",
+			value:       []any{"a", "b", "c"},
+			len:         9,
+			message:     "t2 must have len of 9",
+			isPassed:    false,
+			expectedMsg: "t2 must have len of 9",
+		},
+	}
+
+	v := New()
+
+	for _, test := range tests {
+		v.LenSlice(test.value, test.len, test.field, test.message)
+
+		assert.Equal(t, test.isPassed, v.IsPassed())
+		if v.IsFailed() {
+			assert.Equal(t, test.expectedMsg, v.Errors()[test.field])
+		}
+	}
+}
