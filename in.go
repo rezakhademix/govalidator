6627a6b48f980b8cbd4235a9f7f51c0f5772e56e
Reza Khademi <rezakhademix@gmail.com>
2024-03-07 21:04:32 +0330

    added: missing comment for In method

--- in.go
+++ in.go
@@ -1,5 +1,6 @@
 package validator
 
+// In checks value under validation must be included in the given list of values
 func In[T comparable](value T, acceptableValues ...T) bool {
 	for i := range acceptableValues {
 		if value == acceptableValues[i] {
