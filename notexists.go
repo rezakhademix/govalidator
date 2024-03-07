6ff499950617d413583a887aa915353af32ca4b0
Adel hadadi <hadadiadel81@gmail.com>
2024-03-07 11:14:30 +0330

    fix comments

--- notexists.go
+++ notexists.go
@@ -4,11 +4,11 @@ const (
 	// NotExists represents the rule name which will be used to find the default error message.
 	NotExists = "notExists"
 
-	// NotExistsMsg is default error message format for records that already exist.
+	// NotExistsMsg is default error message format for records that don't exist.
 	NotExistsMsg = "%s already exists"
 )
 
-// NotExists checks value not exists in database.
+// NotExists checks if given value doesn't exist in desired table or not.
 func (v *Validator) NotExists(value any, table, column, field, msg string) *Validator {
 	v.Check(!v.repo.Exists(value, table, column), field, v.msg(NotExists, msg, field))
 
