5d8e0b8e2c4d5cf4447482f9714b046b5e4387c2
Reza Khademi <rezakhademix@gmail.com>
2024-03-08 19:27:27 +0330

    added: when validation rule

---
+++ when_test.go
@@ -0,0 +1,55 @@
+package validator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidator_When(t *testing.T) {
+	tests := []struct {
+		name         string
+		condition    bool
+		expectedExec bool
+	}{
+		{
+			name:         "test the given closure will run if the condition is true",
+			condition:    len("reza") > 0,
+			expectedExec: true,
+		},
+		{
+			name:         "test the given closure will run if the condition is true",
+			condition:    20 == 20,
+			expectedExec: true,
+		},
+		{
+			name:         "test the given closure won't run if the condition is false",
+			condition:    len("reza") < 0,
+			expectedExec: false,
+		},
+		{
+			name:         "test the given closure won't run if the condition is false",
+			condition:    "" == "golang",
+			expectedExec: false,
+		},
+	}
+
+	v := New()
+
+	for _, test := range tests {
+		executed := false
+
+		v.When(test.condition, func() {
+			executed = true
+		})
+
+		assert.Equalf(
+			t,
+			test.expectedExec,
+			executed,
+			"assertion failed, expectedMsg: %s, validatorMsg: %s",
+			test.expectedExec,
+			executed,
+		)
+	}
+}
