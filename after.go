ee358008d608f302757bd0f0c985f88782f58732
Reza Khademi <rezakhademix@gmail.com>
2024-04-04 15:07:48 +0330

    added: after validation rule

---
+++ after.go
@@ -0,0 +1,29 @@
+package govalidator
+
+import "time"
+
+const (
+	// After represents rule name which will be used to find the default error message.
+	After = "after"
+	// AfterMsg is the default error message format for fields with After validation rule.
+	AfterMsg = "%s should be after %v"
+)
+
+// After checks if given time instant t is after u.
+//
+// Example:
+//
+//	v := validator.New()
+//
+//	t, _ := time.Parse("2006-01-02", "2009-01-02") // error ignored for simplicity
+//	u, _ := time.Parse("2006-01-02", "2012-01-01") // error ignored for simplicity
+//
+//	v.After(t, u, "birth_date", "birth_date should be after 2012-01-01.")
+//	if v.IsFailed() {
+//		 fmt.Printf("validation errors: %#v\n", v.Errors())
+//	}
+func (v *Validator) After(t, u time.Time, field, msg string) *Validator {
+	v.Check(t.After(u), field, v.msg(After, msg, field, u))
+
+	return v
+}
--- after_test.go
+++ after_test.go
@@ -7,42 +7,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Before(t *testing.T) {
+func Test_After(t *testing.T) {
 	tests := []struct {
 		name        string
 		field       string
 		value       string
-		beforeValue string
+		afterValue  string
 		isPassed    bool
 		msg         string
 		expectedMsg string
 	}{
 		{
-			name:        "test time instant 2009-11-02 is before than 2012-01-01",
+			name:        "test time instant 2012-01-01 is after 2009-11-02",
 			field:       "birth_date",
-			value:       "2009-11-02",
-			beforeValue: "2012-01-01",
+			value:       "2012-01-01",
+			afterValue:  "2009-11-02",
 			isPassed:    true,
 			msg:         "",
 			expectedMsg: "",
 		},
 		{
-			name:        "test time instant 2019-01-01 isn't before than 2010-01-01 and won't pass validation rule",
+			name:        "test time instant 2010-01-01 isn't after 2019-01-01 and won't pass validation rule",
 			field:       "birth_date",
-			value:       "2019-01-01",
-			beforeValue: "2010-01-01",
+			value:       "2010-01-01",
+			afterValue:  "2019-01-01",
 			isPassed:    false,
 			msg:         "",
-			expectedMsg: "birth_date should be before 2010-01-01 00:00:00 +0000 UTC",
+			expectedMsg: "birth_date should be after 2019-01-01 00:00:00 +0000 UTC",
 		},
 		{
-			name:        "test time instant 2022-02-01 isn't before than 2022-01-01 and won't pass validation rule",
+			name:        "test time instant 2022-01-01 isn't after 2022-02-01 and won't pass validation rule",
 			field:       "birth_date",
-			value:       "2022-02-01",
-			beforeValue: "2022-01-01",
+			value:       "2022-01-01",
+			afterValue:  "2022-02-01",
 			isPassed:    false,
-			msg:         "birth_date can't be before 2022-01-01.",
-			expectedMsg: "birth_date can't be before 2022-01-01.",
+			msg:         "birth_date can't be after 2022-02-01.",
+			expectedMsg: "birth_date can't be after 2022-02-01.",
 		},
 	}
 
@@ -50,9 +50,9 @@ func Test_Before(t *testing.T) {
 
 	for _, test := range tests {
 		value, _ := time.Parse("2006-01-02", test.value)
-		beforeValue, _ := time.Parse("2006-01-02", test.beforeValue)
+		afterValue, _ := time.Parse("2006-01-02", test.afterValue)
 
-		v.Before(value, beforeValue, test.field, test.msg)
+		v.After(value, afterValue, test.field, test.msg)
 
 		assert.Equal(t, test.isPassed, v.IsPassed(), test.name)
 
