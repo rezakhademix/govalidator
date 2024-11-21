f30c1f4421711c4436de3b4acd5831bd6003bce8
Reza Khademi <rezakhademix@gmail.com>
2024-11-21 11:43:57 +0330

    feat: a much faster regex check for email

--- mac.go
+++ mac.go
@@ -1,13 +1,16 @@
 package govalidator
 
+import "regexp"
+
 const (
 	// MAC represents rule name which will be used to find the default error message.
 	MAC = "MAC"
 	// MACMsg is the default error message format for fields with MAC validation rule.
 	MACMsg = "%s is not valid"
-	// MACRegex is the default pattern to validate MAC address field.
-	MACRegex = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
 )
+
+// MACRegex is the default pattern to validate MAC address field.
+var MACRegex = regexp.MustCompile(`^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})`)
 
 // MAC checks the value under validation must match the MACRegex regular expression.
 //
@@ -19,7 +22,7 @@ const (
 //		 fmt.Printf("validation errors: %#v\n", v.Errors())
 //	}
 func (v Validator) MAC(s, field, msg string) Validator {
-	v.RegexMatches(s, MACRegex, field, v.msg(MAC, msg, field))
+	v.check(MACRegex.MatchString(s), field, v.msg(Email, msg, field))
 
 	return v
 }
